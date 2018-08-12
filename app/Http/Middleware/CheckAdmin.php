f84c010011f780df4e66effd8dcc92822a9b1f8b
Gregor Waldvogel <info@gwaldvogel.de>
2018-08-13 01:06:22 +0200

    Added the CheckAdmin middleware

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check() && Auth::user()->admin == true)
+        {
+            return $next($request);
+        }
+        else
+        {
+            if(Auth::user()->admin == null)
+            {
+                Auth::user()->admin = false;
+                Auth::user()->save();
+            }
+            abort(403, __('You do not have the permission to view this page.'));
+        }
+    }
+}
