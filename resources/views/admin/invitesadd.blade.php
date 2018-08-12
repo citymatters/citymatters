5d2a1c98344703ffb90727d6519dc50276c030a7
Gregor Waldvogel <info@gwaldvogel.de>
2018-08-13 01:09:35 +0200

    The field for "forever" when creating an invite is now hidden as it is not used yet

--- resources/views/admin/invitesadd.blade.php
+++ resources/views/admin/invitesadd.blade.php
@@ -10,13 +10,7 @@
             <input type="text" class="form-control" id="code" name="code" aria-describedby="codeHelp" placeholder="{{ __('Enter code') }}">
             <small id="codeHelp" class="form-text text-muted">{{ __('The code users will be able to use to sign up.') }}</small>
         </div>
-        <div class="col-md-6 col-sm-12">
-            <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="foreverCheckbox" name="forever" value="true" checked>
-                <label class="form-check-label" for="foreverCheckbox">{{ __('Valid forever') }}</label>
-                <small id="codeHelp" class="form-text text-muted">{{ __('If you check this, the invite code will be valid forever.') }}</small>
-            </div>
-        </div>
+        <input type="hidden" name="forever" value="true" checked>
         <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
     </form>
 @endsection
