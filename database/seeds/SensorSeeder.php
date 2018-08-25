2d3276813f3bd422f21095d4f52236888f38e5b8
Gregor Waldvogel <info@gwaldvogel.de>
2018-08-25 11:35:44 +0200

    Changed Sensor Seeder

--- database/seeds/SensorSeeder.php
+++ database/seeds/SensorSeeder.php
@@ -15,9 +15,29 @@ class SensorSeeder extends Seeder
      */
     public function run()
     {
+        $org = \App\Organization::where('name', 'city_matters')->first();
+        if(!$org)
+        {
+            $org = new \App\Organization();
+            $org->name = 'city_matters';
+            $org->slug = 'ctymttrs';
+            $org->save();
+        }
+        factory(App\Sensor::class, 50)->create([
+            'organization_id' => $org->_id,
+        ])->each(function ($s) {
+            factory(App\Measpoint::class, 12 * 24 * 30)->create([
+                'sensor_id' => $s->_id,
+            ]);
+        });
+
         $user = \App\User::where('name', 'Gregor')->firstOrFail();
         factory(App\Sensor::class, 50)->create([
             'user_id' => $user->_id,
-        ]);
+        ])->each(function ($s) {
+            factory(App\Measpoint::class, 12 * 24 * 30)->create([
+                'sensor_id' => $s->_id,
+            ]);
+        });
     }
 }
